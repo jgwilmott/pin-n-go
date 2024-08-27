07c1b56e1b34ff539c1b13697b36533c6ed70e9d
jgwilmott <60458668+jgwilmott@users.noreply.github.com>
2024-08-27 00:46:26 -0230

    fix: reorder middleware

--- server/index.ts
+++ server/index.ts
@@ -7,15 +7,17 @@ const app = express();
 dotenv.config();
 connectDB();
 
-// Middleware
+// Request body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
-  res.status(500).json({ message: err.message });
-});
 
 // Routes
 app.use("/flights", flightRouter);
+
+// Error handling middleware
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  res.status(500).json({ message: err.message ?? "Server error" });
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}`));
